5f7106ec74558b3db8d7256651575093d5d8f397
dujunyu <123456>
2020-10-14 23:27:54 +0800

    增加日志系统

--- src/main/java/com/example/github/service/Impl/DemoServiceImpl.java
+++ src/main/java/com/example/github/service/Impl/DemoServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.github.service.Impl;
 
+import com.example.github.Utils.ResultEnpty;
 import com.example.github.dao.TbAreaDao;
 import com.example.github.entity.TbArea;
 import com.example.github.service.DemoService;
-import com.example.github.Utils.ResultEnpty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Service
 public class DemoServiceImpl implements DemoService {
+
+    private final Logger logger =  LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private TbAreaDao tbAreaDao;
@@ -23,8 +27,10 @@ public class DemoServiceImpl implements DemoService {
 
         String s = testDemo("1");
         int i = 10;
-
-
+        for(int j= 1; j <100 ; j++){
+            logger.info("异步日志打印"+ j);
+        }
+        System.out.println("谁先打印");
         return ResultEnpty.success("成功了" ,tbAreas);
     }
 
